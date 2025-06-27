42463f0442d19e0a67987af1ec1655611ba1d73e
John Heilman <1735575+Infro@users.noreply.github.com>
2025-06-27 11:45:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const converter = require('./converters')
 
 let app = {}
 let swaggerObj = {}
-let counter = 0
 /**
  * Hook overview: https://github.com/apidoc/apidoc-core/hooks.md
  */
 module.exports = {
- 
-  init: function(_app) {
+
+  init: function (_app) {
     app = _app;
 
     // Hooks
-    app.addHook('parser-find-elements', parserFindElements)
+    app.addHook('parser-parsed-blocks', parserParsedBlocks)
 
     swaggerObj.info = {
       title: app.packageInfos.title || 'My API',
@@ -31,46 +30,40 @@ module.exports = {
 let pushKey = null
 let pushMethod = null
 
-// iterate over all elements
-function parserFindElements(elements, element, block, filename) {
-  counter++
-  /**
-   * element object properties:
-   * source, name, sourceName, content
-  */
-  try {
-    const elementParser = require(path.join(__dirname, 'node_modules/apidoc-core/lib', app.parsers[element.name]))
-    const parsedElement = elementParser.parse(element.content, element.source)
-
-    if (element.name === 'api') {
-      pushKey = parsedElement.url
-      pushMethod = String(parsedElement.type).toLowerCase()
-      swaggerObj.paths[pushKey] = {}
-      swaggerObj.paths[pushKey][pushMethod] = {}
+const ourKeys = Object.keys(converter.ConvertersMap)
+function parserParsedBlocks(parsedBlocks) {
+  parsedBlocks.forEach((endpoint) => {
+    endpoint.local.type = endpoint.local.type.toLowerCase();
+    swaggerObj.paths[endpoint.local.url] = swaggerObj.paths[endpoint.local.url] || {};
+    if (swaggerObj.paths[endpoint.local.url][endpoint.local.type] !== undefined) {
+      throw new Error(`Endpoint with url ${endpoint.local.url} already exists in swaggerObj.paths. Please check your apidoc configuration.`);
     }
+    swaggerObj.paths[endpoint.local.url][endpoint.local.type] = {};
 
-    // check for supported elements
-    if (element.name && Object.keys(converter.ConvertersMap).includes(element.name)) {
-      swaggerObj.paths[pushKey][pushMethod] = converter.resolve(element.name).init(parsedElement, swaggerObj.paths[pushKey][pushMethod])
+    for (var key in endpoint.local) {
+      swaggerObj.paths[endpoint.local.url][endpoint.local.type].description = endpoint.local.description || '';
+      swaggerObj.paths[endpoint.local.url][endpoint.local.type].tags =
+        (swaggerObj.paths[endpoint.local.url][endpoint.local.type].tags || []).append(endpoint.local.group || 'default');
+
+      // TODO Make rest of swagger object...
+      // Added the code below after sanityCheck to appdoc/lib/core/parser.js:176
+      // app.hook('parser-parsed-blocks', parsedBlocks, self.elements, filename);
     }
-  } catch (err) {
-    console.error('OOPS! errored element: '+JSON.stringify(element.name)+' in filename: '+filename)
-    console.error(err)
-  }
-  return elements;
+  });
 }
 
 process.on('exit', (code) => {
   // triggerd from apidoc when proccess finished successfully
-  // TODO: provide callback when apidoc works finished (this event called only when we use cli)
-  if (code === 0 && counter > 0 && (process.argv.includes('-o') || process.argv.includes('--output'))) {
-    console.log(`[apidoc-plugin-swagger] parse and convert ${counter} element${(counter == 1) ? '' : 's'} to swagger format`)
+  // TODO: provide callback when apidoc works finished (this event called only when we use cli)\
+
+  let outFlagIndex = process.argv.indexOf('-o')
+  if (outFlagIndex === -1) {
+    outFlagIndex = process.argv.indexOf('--output')
+  }
+  if (outFlagIndex !== -1 && process.argv[outFlagIndex + 1]) {
+    console.log(`[apidoc-plugin-swagger] parse and convert to swagger format`)
 
     function getOutputDir() {
-      let outFlagIndex = process.argv.indexOf('-o')
-      if (outFlagIndex === -1) {
-        outFlagIndex = process.argv.indexOf('--output')
-      }
       return process.argv[outFlagIndex + 1]
     }
 
@@ -78,7 +71,7 @@ process.on('exit', (code) => {
     console.log(`[apidoc-plugin-swagger] going to save at path: ${destinationFilePath}`)
 
     fs.writeFileSync(destinationFilePath, JSON.stringify(swaggerObj, null, 2), 'utf8')
-    
+
     console.log(`[apidoc-plugin-swagger] swagger.json spec file saved successfully`)
   }
 })
